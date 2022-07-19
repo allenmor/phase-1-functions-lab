ed3bc43838581e19fd34b8f24f201fccb37b7dbb
Allen Mordukhaev <allenmor93@gmail.com>
2022-07-18 23:13:33 -0400

    commit tree d21251ce2c1e67a04ffa6b339fbec13758f7b18f

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(a) {
+    if (a > 42){
+        return a - 42;
+    } else {
+        return 42 - a;
+    }
+}
+
+function distanceFromHqInFeet(b) {
+    return distanceFromHqInBlocks(b) * 264;
+}
+
+function distanceTravelledInFeet(c, d) {
+    return distanceFromHqInFeet(d) - distanceFromHqInFeet(c);
+}
+
+function calculatesFarePrice(c, d) {
+    if (distanceTravelledInFeet(c, d) <= 400) {
+        return 0
+    } else if (distanceTravelledInFeet(c, d) > 400
+    && distanceTravelledInFeet(c, d) <= 2000) {
+        return (distanceTravelledInFeet(c, d) - 400) * .02
+
+    } else if (distanceTravelledInFeet(c, d) > 2000 
+    && distanceTravelledInFeet(c, d) < 2500) {
+        return 25
+    } else {
+        return 'cannot travel that far'
+    }
+}
+
